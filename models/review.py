f16739ec44e095de5b8f14c79361ccd5228f65e6
jhonaRiver <jhonatan.rivera.pr@gmail.com>
2022-04-11 18:24:16 +0000

    Updating files for review

--- models/review.py
+++ models/review.py
@@ -1,10 +1,20 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
 from models.base_model import BaseModel
+from models.base_model import Base
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+from os import getenv
 
 
-class Review(BaseModel):
+class Review(BaseModel, Base):
     """ Review classto store review information """
-    place_id = ""
-    user_id = ""
-    text = ""
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        __tablename__ = "reviews"
+        place_id = Column(String(60), ForeignKey("place.id"), nullable=False)
+        user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
+        text = Column(String(1024), nullable=False)
+    else:
+        place_id = ""
+        user_id = ""
+        text = ""
